64f9846228471a92abf008c1d4ab3d169bccd07b
Cade Duncan <cadeduncan@Cades-MacBook-Air.local>
2022-05-18 17:05:41 -0700

    yes

--- StingBot/main.py
+++ StingBot/main.py
@@ -11,7 +11,6 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-
 extensions = []
 
 # Get configuration.json
